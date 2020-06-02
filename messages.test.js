117e7c47739adb24c4f1b433e7e81c585ad297be
Ester Daniel Ytterbrink <esterdaniel.ytterbrink@eficode.com>
2020-06-02 11:29:18 +0200

    It is an async call

--- messages.test.js
+++ messages.test.js
@@ -1,9 +1,10 @@
 const messages = require('./messages')
 
-test('adds an emoji', () => {
+test('adds an emoji', async () => {
   const posts = []
-  const web = { reactions: { add: (options) => { posts.push(options) } } }
-  messages.addEmoji(['poop'], 'retro', '1234', web)
+  const web = { reactions: { add: async (options) => { posts.push(options) } } }
+  expect.assertions(1)
+  await messages.addEmoji(['poop'], 'retro', '1234', web)
   expect(posts).toStrictEqual([{
     channel: 'retro',
     timestamp: '1234',
